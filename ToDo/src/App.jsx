90f9f63891a058229ed4607f970f1a53291ab5df
Roberto Neto <robertocorreamoreiraneto@gmail.com>
2023-10-02 14:05:48 -0300

    [ToDo] - Criando esqueleto do ToDo List

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,10 +1,41 @@
-import {  } from 'react'
+import { useState } from 'react'
 
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Estudar Web hacking",
+      category: "Estudos",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Estudar Docker",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
 
   return (
-    <div>Olá Roberto Neto</div>
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className='todo-list'>
+          {todos.map((todo) => (
+            <div key={todo.id} className='todo'>
+              <div className='content'>
+                <p>{todo.text}</p>
+                <p className='category'>({todo.category})</p>
+              </div>
+              
+              <div>
+                <button>Completar</button>
+                <button>x</button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
   )
 }
 
