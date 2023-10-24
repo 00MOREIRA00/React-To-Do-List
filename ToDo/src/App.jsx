91788103f89289b87019971025e9cb2f8c336987
Roberto Neto <robertocorreamoreiraneto@gmail.com>
2023-10-23 21:49:25 -0300

    [ToDo] - Adicionando funcionalidade de categorias

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -23,6 +23,8 @@ function App() {
   ])
 
   const [search, setSearch] = useState("")
+  const [filter, setFilter] = useState("All")
+  const [sort, setSort] = useState("Asc")
 
   const addToDo = (text, category) => {
     const newToDos = [...todos, {
@@ -51,9 +53,9 @@ function App() {
     <div className='app'>
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter}/>
       <div className='todo-list'>
-          {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+          {todos.filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted).filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
             <ToDo todo={todo} key={todo.id} removeTodo={removeTodo} completeTodo={completeTodo}/>
           ))}
       </div>
--- ToDo/src/components/filter.jsx
+++ ToDo/src/components/filter.jsx
@@ -1,13 +1,13 @@
 
 
-const filter = () => {
+const filter = ( {filter, setFilter }) => {
   return (
     <div className="filter">
         <h2>Filtrar:</h2>
         <div className="filter-options">
             <div>
                 <p>Status:</p>
-                <select>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
                     <option value="All">Todas</option>
                     <option value="Completed">Completas</option>
                     <option value="Incompleted">Incompletas</option>
